c4f25ba15d303a6acf1d26b8ff3d47d8b37fa885
DitijTanwar29 <ditijtanwar2001@gmail.com>
2024-05-23 15:15:11 +0530

    mail sender added with more job apis

--- models/Twilio.js
+++ models/Twilio.js
@@ -1,48 +1,31 @@
 // models/twilio.js
 
-const twilio = require('twilio');
+// const twilio = require('twilio');
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-const client = twilio(accountSid, authToken);
+// const client = twilio(accountSid, authToken);
 
-const sendSMS = async (to, body) => {
-  try {
-    const message = await client.messages.create({
-      body,
-      from: process.env.TWILIO_PHONE_NUMBER,
-      to,
-    });
-    console.log("inside twilio")
+// const sendSMS = async (to, body) => {
+//   try {
+//     const message = await client.messages.create({
+//       body,
+//       from: process.env.TWILIO_PHONE_NUMBER,
+//       to,
+//     });
+//     console.log("inside twilio")
 
     
-    console.log(`Message sent: ${message.sid}`);
-  } catch (error) {
-    console.error(`Error sending SMS: ${error.message}`);
-    throw error;
-  }
-};
+//     console.log(`Message sent: ${message.sid}`);
+//   } catch (error) {
+//     console.error(`Error sending SMS: ${error.message}`);
+//     throw error;
+//   }
+// };
 
-// client.verify.v2
-//     .services(accountSid)
-//     .verifications.create({ to: "+919911553407", channel: "sms" })
-//     .then((verification) => console.log(verification.status))
-//     .then(() => {
-//       const readline = require("readline").createInterface({
-//         input: process.stdin,
-//         output: process.stdout,
-//       });
-//       readline.question("Please enter the OTP:", (otpCode) => {
-//         client.verify.v2
-//           .services(accountSid)
-//           .verificationChecks.create({ to: "+919911553407", code: otpCode })
-//           .then((verification_check) => console.log(verification_check.status))
-//           .then(() => readline.close());
-//       });
-//     });
 
-module.exports = { sendSMS };
+// module.exports = { sendSMS };
 
 
 
